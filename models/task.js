3785934a77755e9750e3d0d34106be71e43da5ca
emmanuel <eolaojo@yahoo.com>
2016-06-21 11:37:28 -0400

    Task.run returns promise. Added index and state to task model. + other changes

--- models/task.js
+++ models/task.js
@@ -2,29 +2,23 @@
  * This module dictates the structure of a
  * task.
  */
-var mongoose =  require('mongoose'),
-  Schema = mongoose.Schema;
-mongoose.connect('mongodb://127.0.0.1:27017/SurveyDB');
-var _ = require("lodash");
+var mongoose =  require('mongoose');
+var Schema = mongoose.Schema;
 
 /**
  * The taskSchema is basically an object that contains
  * a user and one other user that they follow
  */
 var taskSchema = new Schema({
-  name: {type: String, required: true}
-  , start: {type: String, required: true}
-  , clean: {type: String, required: true}
-  , destroy: {type: String, required: true}
+  steps: [{
+    type: {type: String, required: true}
+    , index: {type: Number, required: true}
+    , state: {type: String, required: true}
+    , name: {type: String, required: true}
+    , condition: String
+    , data: {}
+  }]
 });
-
-var model = mongoose.model("task", taskSchema);
-
-var task = new model();
-task.name = "save";
-
-console.log(task.constructor.modelName);
-console.log(JSON.parse(JSON.stringify(task)));
 
 module.exports = function(collection){
   return mongoose.model(collection, taskSchema);

8bcfb8c33aeff36fa82c2866db49abc93d8b158f
emmanuel <eolaojo@yahoo.com>
2016-06-22 18:21:31 -0400

    changed step implementation to allow model names instead of model object

---
+++ test_conf.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db: "mongodb://127.0.0.1:27017/"
+};
--- tests/task.js
+++ tests/task.js
@@ -2,3 +2,25 @@
  * @author EmmanuelOlaojo
  * @since 6/22/16
  */
+
+var chai = require("chai");
+var expect = chai.expect;
+chai.use(require("chai-as-promised"));
+
+var config = require("../test_conf");
+var Lint = require("../lib/lint");
+var lint = new Lint(config.db + "SurveyDB", "_TASKS");
+var Task = lint.Task;
+
+var task = new Task();
+var Response = task.getCollection("responses");
+
+describe("Task", function(){
+  it("should return a list of responses", function () {
+    expect(Response.find()).to.eventually.have.length(5);
+  });
+
+  it("should successfully complete all steps", function(){
+    
+  });
+});

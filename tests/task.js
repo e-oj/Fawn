4fa74d811fd8fb8a1b7e8d5ee19ca80f5fe90f9f
emmanuel <eolaojo@yahoo.com>
2016-06-23 11:01:24 -0400

    Code cleanup. Started testing.

--- tests/task.js
+++ tests/task.js
@@ -9,18 +9,42 @@ chai.use(require("chai-as-promised"));
 
 var config = require("../test_conf");
 var Lint = require("../lib/lint");
-var lint = new Lint(config.db + "SurveyDB", "_TASKS");
-var Task = lint.Task;
-
-var task = new Task();
-var Response = task.getCollection("responses");
+var DB = "test";
+var TASKS = "LINT";
+var TEST_COLLECTION = "people";
+var lint;
+var Task;
+var testMdl;
+var taskMdl;
+var task;
 
 describe("Task", function(){
-  it("should return a list of responses", function () {
-    expect(Response.find()).to.eventually.have.length(5);
+  before(function(){
+    lint = new Lint(config.db + DB, TASKS);
+    Task = lint.Task;
+    task = new Task();
   });
 
-  it("should successfully complete all steps", function(){
-    
+  after(function(done){
+    task.dropCollection(TEST_COLLECTION).then(function(){
+      done();
+    })
   });
+  
+  it("should save successfully and delete tasks", function(done){
+    task.save(TEST_COLLECTION, {name: "Emmanuel Olaojo", age: 20});
+    task.save(TEST_COLLECTION, {name: "John Damos", age: 26});
+    task.run()
+      .then(function(){
+        testMdl = task.getCollection(TEST_COLLECTION);
+        taskMdl = task.getTaskCollection();
+
+        expect(testMdl.find()).to.eventually.have.length(2);
+        expect(taskMdl.find()).to.eventually.have.length(0);
+        done();
+      })
+      .catch(done);
+  });
+
+  
 });
